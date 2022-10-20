918289006f18cea0bcfdff2bd0713c4ef19a8436
aronzvi <aronzvi@users.noreply.github.com>
2022-10-20 09:21:20 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    /*trace: 'retain-on-failure',*/
+    trace: 'retain-on-failure',
     video: 'retain-on-failure', // Not working if we create the context manually as we do. The below works but overrides mode
     /* 
     contextOptions: {
@@ -48,7 +48,7 @@ const config: PlaywrightTestConfig = {
       }
     },
     */
-    /*screenshot: 'only-on-failure',*/
+    //screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
