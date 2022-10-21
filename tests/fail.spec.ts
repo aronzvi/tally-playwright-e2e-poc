e94fbaac16c3eb6dbb646094a6360372764b4783
aronzvi <aronzvi@aronzvis-MacBook-Pro.local>
2022-10-21 16:53:27 -0300

    move extension location to dist

--- tests/fail.spec.ts
+++ tests/fail.spec.ts
@@ -6,7 +6,7 @@ export const test = base.extend<{
   extensionId: string;
 }>({
   context: async ({ }, use) => {
-    const pathToExtension = path.join(__dirname, "chrome");
+    const pathToExtension = path.resolve(__dirname,'../dist/chrome');
     const context = await chromium.launchPersistentContext("", {
       headless: false,
       args: [
